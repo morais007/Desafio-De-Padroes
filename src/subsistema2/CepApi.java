7e700532305d4937aff7d2b87383d5cd75f4aee4
morais007 <SandroMorais>
2024-07-02 21:25:34 -0300

    DesafioDePadroes

---
+++ src/subsistema2/CepApi.java
@@ -0,0 +1,22 @@
+package subsistema2;
+
+public class CepApi {
+
+	private static CepApi instancia = new CepApi();
+
+	private CepApi() {
+		super();
+	}
+
+	public static CepApi getInstancia() {
+		return instancia;
+	}
+	
+	public String recuperarCidade(String cep) {
+		return "Fortaleza";
+	}
+	
+	public String recuperarEstado(String cep) {
+		return "CE";
+	}
+}
